086c847f21364940411d43bdf8fdded800227c35
Devin Brown <devin.g.brown@gmail.com>
2015-06-13 12:54:50 -0600

    add fortune finding logic

--- EightBall/ViewController.swift
+++ EightBall/ViewController.swift
@@ -20,6 +20,34 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet var answerView: UITextView!
 
+    let answers = [ "\rYES", "\rNO", "\rMAYBE", "I\rDON'T\rKNOW",
+        "TRY\rAGAIN\rSOON", "READ\rTHE\rMANUAL" ]
+
+
+    func fadeFortune() {
+        fadeOutAnswerView()
+    }
+
+    func newFortune() {
+        let randomIndex = Int(arc4random_uniform(UInt32(answers.count)))
+        answerView.text = answers[randomIndex]
+        fadeInAnswerView()
+    }
+
+    // Internal Functions
+
+    func fadeOutAnswerView() {
+        UIView.animateWithDuration(0.75) {
+            self.answerView.alpha = 0.0
+        }
+    }
+
+    func fadeInAnswerView() {
+        UIView.animateWithDuration(2.0) {
+            self.answerView.alpha = 1.0
+        }
+    }
 }
 
